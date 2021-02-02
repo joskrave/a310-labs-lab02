04ee5125f63707eea21151f214002fac46d7c162
joskrave <joskrave@iu.edu>
2021-02-01 21:43:08 -0500

    Update Queue.py
    updated queue

--- Queue.py
+++ Queue.py
@@ -1,15 +1,17 @@
+from LinkedList import *
+
 class Queue:
   def __init__(self): # constructor
-    self.contents = []
+    self.contents = MTList()
   def isEmpty(self): # predicate to test contents 
-    return self.contents == []
+    return self.contents.isEmpty()
   def front(self): # lookup function 
-    return self.contents[0]
+    return self.contents.first()
   def enqueue(self, element): # add elements 
-    self.contents = self.contents + [ element ]
+    self.contents = self.contents.append(NEList(element))
   def dequeue(self): # take elements out 
     item = self.front()
-    self.contents = self.contents[1:]
+    self.contents = self.contents.rest()
     return item
   def __str__(self): # functions to simplify viewing 
     return str(self.contents)
@@ -18,20 +20,19 @@ class Queue:
 
 def main():
   a = Queue()            # creates an empty queue 
-  print(a)               # prints [] 
-  print(a.isEmpty())     # prints True 
+  print(str(a) + " is the empty queue")               # prints [] 
+  print(str(a.isEmpty()) + " should print True")     # prints True 
   a.enqueue(1)           # prints nothing but adds 1 to the queue 
-  print(a)               # prints [1]
-  print(a.front())       # prints 1
-  print(a)               # prints [1]
+  print(str(a) + " if we add 1 to the queue")               # prints [1]
+  print(str(a.front()) + " prints 1 since that's the front")       # prints 1
+  print(str(a) + " is a queue with one element")               # prints [1]
   a.enqueue(5)           # prints nothing the queue is now [1, 5]
   a.enqueue(8)           # prints nothing the queue is now [1, 5, 8]
   a.enqueue(10)          # prints nothing the queue is now [1, 5, 8, 10]
-  print(a.isEmpty())     # prints False  
-  print(a)               # prints [1, 5, 8, 10]
-  print(a.dequeue())     # prints 1 and the queue is now [5, 8, 10]
-  print(a)               # prints [5, 8, 10]
+  print(str(a.isEmpty()) + " since the queue is not empty")     # prints False  
+  print(str(a) + " is [1, 5, 8, 10]")               # prints [1, 5, 8, 10]
+  print(str(a.dequeue()) + " is 1") # prints 1 and the queue is now [5, 8, 10]
+  print(str(a)+ " is [5, 8, 10]")               # prints [5, 8, 10]
 
 if __name__=="__main__":
   main()
-  
